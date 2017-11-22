d324eaa54159b7ec8c1ebdccf1fe815861d1706c
ZMlWxmArSN <automate1@wavemaker.com>
2017-11-22 10:22:35 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_xNnRTwHdOk",
+  "homePage" : "Main",
+  "name" : "Auto_xNnRTwHdOk",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
